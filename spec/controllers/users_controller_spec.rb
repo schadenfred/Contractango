13fb8b154fca5bd639d3743cd08ed71dccf6be54
Fred Schoeneman <fred.schoeneman@gmail.com>
2010-07-12 22:08:56 -0700

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -33,6 +33,19 @@ describe UsersController do
   end
 
   describe "GET 'new'" do
+
+    it "should be successful" do
+      get :new
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :new
+      response.should have_selector("title", :content => "Sign up")
+    end
+  end
+
+  describe "GET 'new'" do
     it "should be successful" do
       get 'new'
       response.should be_success
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,20 +9,17 @@ describe "Users" do
       it "should not make a new user" do
         lambda do
           visit signup_path
+          fill_in "Name",         :with => ""
+          fill_in "Email",        :with => ""
+          fill_in "Password",     :with => ""
+          fill_in "Confirmation", :with => ""
           click_button
-          response.should render_template('users/new')
           response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
       end
     end
 
     describe "success" do
-
-      after(:each) do
-        # Integration tests should roll back the database changes,
-        # but the don't, so this is the workaround.
-        User.delete_all
-      end
 
       it "should make a new user" do
         lambda do
